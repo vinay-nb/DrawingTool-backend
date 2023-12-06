3ac05d66c0f8dd470eb965830aabb8cb45283dc9
Vinay Bhat <vinaybhathg11@gmail.com>
2023-12-06 22:43:34 +0530

    feat:emit-consume-events

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
   console.log("server connection successful");
+
+  socket.on("beginPath", (arg) => {
+    socket.broadcast.emit("beginPath", arg);
+  });
+
+  socket.on("drawLine", (arg) => {
+    socket.broadcast.emit("drawLine", arg);
+  });
+
+  socket.on("changeConfig", (arg) => {
+    socket.broadcast.emit("changeConfig", arg);
+  });
 });
 
 httpServer.listen(5000);
